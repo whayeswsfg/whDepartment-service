0330a9774c38df49f34e2b90af67093780270c6a
Wanda <whayeswsfg@gmail.com>
2021-01-31 21:59:17 -0500

    WH Adding test and committing

--- src/test/java/com/whayes/services/whDepartmentservice/WhDepartmentServiceApplicationTests.java
+++ src/test/java/com/whayes/services/whDepartmentservice/WhDepartmentServiceApplicationTests.java
@@ -8,6 +8,7 @@ class WhDepartmentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
---
+++ src/test/java/com/whayes/services/whDepartmentservice/service/DepartmentServiceTest.java
@@ -0,0 +1,30 @@
+package com.whayes.services.whDepartmentservice.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveDepartment() {
+        assertFalse(false);
+    }
+
+    @Test
+    void findDepartmentByID() {
+        //departmentRepository.findByDepartmentID(departmentID);
+       // long departmentID = this.
+        assertTrue(true);
+    }
+}
